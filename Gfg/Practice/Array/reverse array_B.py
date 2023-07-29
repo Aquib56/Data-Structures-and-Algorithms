950dc07db6087eac31979107feafcbc34b6509bf
Aquib <aquibs732@gmail.com>
2023-07-29 18:16:27 +0530

    a

--- Gfg/Practice/Array/reverse array_B.py
+++
@@ -1,7 +0,0 @@
-def reverse(nums):
-    res=[]
-    for i in range(len(nums)-1,-1,-1):
-        res.append(nums[i])
-    return res
-arr=[3,1,54,6,2]
-print(reverse(arr))
--- Gfg/Practice/Array/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-    # subtotal = calculate_subtotal(order)
-    # print("Subtotal for the order is: " + str(subtotal))
-
-    # tax = calculate_tax(subtotal)
-    # print("Tax for the order is: " + str(tax))
